badfabac260c63d5bb750e9c8d8ffec42a361f10
bit <bit@DESKTOP-518UE4O>
2018-03-26 15:34:03 +0900

    FileUpload Exercise

--- src/com/example/fileupload/controller/FileUploadController.java
+++ src/com/example/fileupload/controller/FileUploadController.java
@@ -7,16 +7,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.example.fileupload.service.FileUploadService;
+
 @Controller
 public class FileUploadController {
-	
-	@RequestMapping( "/form" )
-	public String form() {
-		return "form";
-	}
-	
-	@RequestMapping( "/upload" )
-	public String upload() {
-		return "result";
-	}
+    @Autowired
+    FileUploadService fileUploadService;
+    
+    @RequestMapping( "/form" )
+    public String form() {
+	return "form";
+    }
+
+    @RequestMapping( "/upload" )
+    public String upload( @RequestParam( "email" ) String email, @RequestParam( "file1" ) MultipartFile multipartFile,
+	    Model model ) {
+
+	String url = fileUploadService.restore( multipartFile );
+	model.addAttribute( "url", url );
+	return "result";
+    }
 }
